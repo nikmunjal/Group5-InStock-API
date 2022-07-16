d8fcf226bfb3729803623bacec4c781b15a6a5b5
andrewssito <aksito1102@gmail.com>
2022-07-16 12:39:32 -0700

    get request for single inventory item

--- routes/inventoryRoutes.js
+++ routes/inventoryRoutes.js
@@ -8,6 +8,13 @@ const warehouseData = require( '../data/warehouses.json' );
 
 router.get('/',(req, res) =>
 res.status(200).send(inventoryData)
-)
+);
+
+//      GET A SINGLE INVENTORY ITEM
+router.get('/:id', (req, res) => {
+    const {id} = req.params;
+    const itemDetails = inventoryData.find (( {id:itemId} ) => id === itemId );
+    return res.json(itemDetails);
+})
 
 module.exports = router;
