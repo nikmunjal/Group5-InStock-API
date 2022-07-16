671f0bd7cdeebf75b1610efbd8de28aa38b5a694
Nik <nikmunjal@gmail.com>
2022-07-16 13:10:37 -0400

    initial project structure and server set up

---
+++ routes/warehouseRoutes.js
@@ -0,0 +1,13 @@
+const express = require( 'express' );
+const router = express.Router();
+const { v4: uuidv4 } = require('uuid');
+const fs = require('fs');
+router.use(express.json());
+const inventoryData = require( '../data/inventories.json' );
+const warehouseData = require( '../data/warehouses.json' );
+
+router.get('/',(req, res) =>
+res.status(200).send(warehouseData)
+)
+
+module.exports = router;
