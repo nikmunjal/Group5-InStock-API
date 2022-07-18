e9b4288b80182616fe82bcdcc607661845b4ce90
JumaMac <mjustinmacaraig@gmail.com>
2022-07-18 02:03:57 -0400

    GET request for inventories of warehouses by their ID

--- routes/warehouseRoutes.js
+++ routes/warehouseRoutes.js
@@ -21,4 +21,31 @@ router.get('/:id', (req, res) => {
     else res.status(404).send("That warehouse was not found");
 });
 
+// grabbing the inventories of warehouses
+
+router.get("/:id/inventory", (req, res) => {
+            const findInv = inventoryData.find((inv) => inv.warehouseID === req.params.id);
+        if (findInv) {
+            const warehouseInv = inventoryData
+            .filter((item) => item.warehouseID === req.params.id)
+            .map((inv) => {
+                return {
+                id: inv.id,
+                warehouseID: inv.warehouseID,
+                warehouseName: inv.warehouseName,
+                itemName: inv.itemName,
+                description: inv.description,
+                category: inv.category,
+                status: inv.status,
+                quantity: inv.quantity,
+                };
+            });
+            res.json(warehouseInv);
+        } else {
+            return res.status(404).send("warehouse has no inventory");
+        }
+    }
+);
+
+
 module.exports = router;
